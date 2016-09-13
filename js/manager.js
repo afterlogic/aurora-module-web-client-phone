0c7b59aa03a61a10b9de1d414f0aa3fdff4c30b1
nadine-shi <nadine@afterlogic.com>
2016-09-13 17:28:20 +0300

    System - use manager.js files instead of manager-smth.js

--- js/manager.js
+++ js/manager.js
@@ -1,27 +1,31 @@
 'use strict';
 
 module.exports = function (oAppData) {
-	require('modules/%ModuleName%/js/enums.js');
-
-	var
-		Browser = require('%PathToCoreWebclientModule%/js/Browser.js'),
-		App = require('%PathToCoreWebclientModule%/js/App.js'),
-
-		Settings = require('modules/%ModuleName%/js/Settings.js'),
-		oSettings = oAppData['%ModuleName%'] || {}
-	;
+	var App = require('%PathToCoreWebclientModule%/js/App.js');
 	
-	Settings.init(oSettings);
+	if (App.getUserRole() === Enums.UserRole.NormalUser)
+	{
+		require('modules/%ModuleName%/js/enums.js');
+
+		var
+			Browser = require('%PathToCoreWebclientModule%/js/Browser.js'),
+			App = require('%PathToCoreWebclientModule%/js/App.js'),
+
+			Settings = require('modules/%ModuleName%/js/Settings.js'),
+			oSettings = oAppData['%ModuleName%'] || {}
+		;
+
+		Settings.init(oSettings);
+
+		return {
+			getHeaderItem: function () {
+				return (!Browser.ie && !App.isMobile()) ? {
+					item: require('modules/%ModuleName%/js/views/PhoneView.js'),
+					name: Settings.HashModuleName
+				} : null;
+			}
+		};
+	}
 	
-	return {
-		isAvailable: function (iUserRole, bPublic) {
-			return !bPublic && iUserRole === Enums.UserRole.NormalUser;
-		},
-		getHeaderItem: function () {
-			return (!Browser.ie && !App.isMobile()) ? {
-				item: require('modules/%ModuleName%/js/views/PhoneView.js'),
-				name: Settings.HashModuleName
-			} : null;
-		}
-	};
+	return null;
 };
